46b7ad32b91a9e4b59003e86301cfb0f0eb9c25a
Victor Correa Ruiz <vcorrear@gamma30.aulas.gsyc.urjc.es>
2018-11-19 10:50:18 +0100

    Cliente y servidor modificados hasta confirmacion de comunicacion entre ambos

--- client.py
+++ client.py
@@ -5,26 +5,30 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
-
+import sys
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+try:
+    METHOD = sys.argv[1]
+    USER = sys.argv[2].split(":")[0]
+    PORT = sys.argv[2].split(":")[1]
+    SERVER = USER.split("@")[1]
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
-
+    # Contenido que vamos a enviar
+    LINE = (METHOD + " sip:" + USER + " SIP/2.0\r\n")
+    
+except IndexError:
+    sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
+    
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+    my_socket.connect((SERVER, int(PORT)))
 
     print("Enviando: " + LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-
-    print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
-
-print("Fin.")
+    print(data.decode('utf-8'))
+    my_socket.send(bytes("ACK sip:" + USER + " SIP/2.0","utf-8") + b'\r\n')
+    print("Fin.")
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
-
+import sys
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -14,18 +14,18 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
-
+        line = self.rfile.read().decode('utf-8')
+        contenido = line.split()
+        print("El cliente nos manda " + line)
+        if contenido[0] == "INVITE":
+            self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n" 
+                              + b"SIP/2.0 180 Ringing\r\n\r\n"
+                              + b"SIP/2.0 200 OK\r\n")
+        
 if __name__ == "__main__":
+    
+    PORT = sys.argv[2]
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer(('', int(PORT)), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
