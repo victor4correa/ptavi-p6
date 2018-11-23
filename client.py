bdf76b5553c2fb00a7ebcd968322df73032f58ab
Victor Correa Ruiz <vcorrear@alpha34.aulas.gsyc.urjc.es>
2018-11-23 12:04:00 +0100

    Cliente y servidor terminados y pasado pep8 y pep257 falta filtar captura

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente que abre un socket a un servidor
-"""
+"""Programa cliente que abre un socket a un servidor."""
 
 import socket
 import sys
@@ -17,10 +15,10 @@ try:
 
     # Contenido que vamos a enviar
     LINE = (METHOD + " sip:" + USER + " SIP/2.0\r\n")
-    
+
 except IndexError:
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
-    
+
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -31,6 +29,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     data = my_socket.recv(1024)
     print(data.decode('utf-8'))
     if METHOD == "INVITE":
-        my_socket.send(bytes("ACK sip:" + USER + " SIP/2.0","utf-8") + b'\r\n')
+        my_socket.send(bytes("ACK sip:" + USER + " SIP/2.0", "utf-8")
+                       + b'\r\n')
         data = my_socket.recv(1024)
     print("Fin.")
--- server.py
+++ server.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de eco en UDP simple
-"""
+"""Clase (y programa principal) para un servidor de eco en UDP simple."""
 
 import socketserver
 import sys
 import os
 
+
 class EchoHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+    """Echo server class."""
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
+
         line = self.rfile.read().decode('utf-8')
         contenido = line.split()
         print("El cliente nos manda " + line)
-        if contenido[0] == "INVITE":
-            self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n" 
-                              + b"SIP/2.0 180 Ringing\r\n\r\n"
-                              + b"SIP/2.0 200 OK\r\n") 
-        elif contenido[0] == "ACK":
-                os.system('mp32rtp -i 127.0.0.1 -p 23032 < ' + FILE)
-        elif contenido[0] == "BYE":
-            self.wfile.write(b"Terminando llamada\r\n\r\n")
-        elif contenido[0] != ["INVITE","BYE","ACK"]:
-            self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
-        
+        if len(contenido) != 3:
+            self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+        else:
+            if contenido[0] == "INVITE":
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+                self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            elif contenido[0] == "ACK":
+                    os.system("mp32rtp -i 127.0.0.1 -p 23032 < " + FILE)
+            elif contenido[0] == "BYE":
+                self.wfile.write(b"Terminando llamada\r\n\r\n")
+            elif contenido[0] != ["INVITE", "BYE", "ACK"]:
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+
 
 if __name__ == "__main__":
-    
+
     try:
         PORT = sys.argv[2]
         FILE = sys.argv[3]
